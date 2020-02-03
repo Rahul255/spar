75ca33ad7982d9526abf68f677be3b30952715ac
Rahul255 <rahulmohan255@gmail.com>
2020-02-03 15:44:02 +0530

    Home.js added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import  Main from "./Main";
+
 
 ReactDOM.render(
-  <App />,
+  <Main/>,
   document.getElementById('root')
 );
